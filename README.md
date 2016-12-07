6685871150a95f6a6a9f332ba4f31233e946b265
Victor Kulebyakin <victor@pingbull.com>
2016-12-07 15:45:29 +0200

    add info to readme

--- README.md
+++ README.md
@@ -2,10 +2,25 @@
 
 JavaScript implementation of WordPress PHP function. Converts selected email addresses characters to HTML entities to block spam bots. Not all characters in the email address are converted: the selection is random and changes each time the function is called.
 
+## Installation
+```
+$ npm i --save antispambotjs
+```
+
 ## Usage
 
 ```
 antispambot(emailAddress, hexEncoding);
+```
+
+## Example
+
+```
+import antispambot from 'antispambotjs';
+
+let encoded = antispambot('test@test.com');
+
+console.log(encoded); // te&#115;t&#64;&#116;es&#116;.c&#111;&#109;
 ```
 
 ## License
--- test/test.js
+++ test/test.js
@@ -5,9 +5,10 @@ import antispambot from '../src';
 let entities = new AllHtmlEntities();
 
 describe('antispambot', () => {
-    let encoded = antispambot('test@test.com');
+    let email = 'test@test.com';
+    let encoded = antispambot(email);
 
-    it('should not be equal to input string', () => expect(encoded).to.not.equal('test@test.com'));
+    it('should not be equal to input string', () => expect(encoded).to.not.equal(email));
     it('should replace & with HTML entity', () => expect(encoded).to.contain('&#64;'));
-    it('should be equal to input string', () => expect(entities.decode(encoded)).to.equal('test@test.com'));
+    it('should be equal to input string', () => expect(entities.decode(encoded)).to.equal(email));
 });
